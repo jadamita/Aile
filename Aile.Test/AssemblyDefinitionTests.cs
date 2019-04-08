cf14b8cce726f18e493960ec8b3c77b7baefc0c8
Joseph <jadamita@itouchorlando.com>
2019-04-08 16:55:00 -0400

    added example unit test

---
+++ Aile.Test/AssemblyDefinitionTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Aile.Test
+{
+    [TestClass]
+    public class AssemblyDefinitionTests
+    {
+        // Testing methods should follow: <method>_Should<expected>_When<condition>
+
+        [TestMethod]
+        public void Constructor_ShouldThrow_WhenInvalid()
+        {
+            FileNotFoundException expectedException = null;
+
+            try
+            {
+                AssemblyDefinition assemblyDefinition = new AssemblyDefinition("Does_Not_Exist.exe");
+            }
+            catch (FileNotFoundException ex)
+            {
+                expectedException = ex;
+            }
+
+            Assert.IsNotNull(expectedException);
+        }
+
+        [TestMethod]
+        public void Constructor_ShouldParse_WhenValid()
+        {
+            AssemblyDefinition assemblyDefinition = new AssemblyDefinition("Aile.TestFile.exe");
+            Assert.AreEqual(assemblyDefinition.GetFileLength(), 4608);
+        }
+    }
+}
--- Aile/AssemblyDefinition.cs
+++ Aile/AssemblyDefinition.cs
@@ -1,11 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
+using System.Diagnostics;
 
 namespace Aile
 {
     public class AssemblyDefinition
     {
+        private string _assemblyFilePath;
+
+        private long _streamLength;
+
+        private BinaryReader _binaryReader;
+
+        public AssemblyDefinition(string assemblyFilePath)
+        {
+            try
+            {
+                _binaryReader = new BinaryReader(File.Open(assemblyFilePath, FileMode.Open));
+                
+                if (_binaryReader.BaseStream.Length > 0)
+                    this._streamLength = _binaryReader.BaseStream.Length;
+            }
+            catch (FileNotFoundException)
+            {
+                throw;
+            }
+        }
+
+        public BinaryReader GetBinaryReader()
+        {
+            return this._binaryReader;
+        }
+
+        public long GetFileLength()
+        {
+            return (this._streamLength);
+        }
 
     }
 }
